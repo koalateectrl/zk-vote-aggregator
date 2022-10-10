6ede7bdee1e770d0730285393010bc1fe465c9b1
Samuel Wong <sandymule@gmail.com>
2022-10-10 13:20:09 -0700

    copy over relevant files from zk-example

---
+++ contracts/ZK.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.11;
+
+import "./InitVerifier.sol";
+
+contract ZK is Verifier {
+    uint256 public hash =
+        uint256(
+            15893827533473716138720882070731822975159228540693753428689375377280130954696
+        );
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function isPreImage(
+        uint256[2] memory a,
+        uint256[2] memory b_0,
+        uint256[2] memory b_1,
+        uint256[2] memory c,
+        uint256[1] memory inputs // public inputs
+    ) public view returns (bool) {
+        require(hash == inputs[0], "wrong hash");
+        uint256[1] memory publicInputs = inputs;
+        return verifyProof(a, [b_0, b_1], c, publicInputs);
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,38 @@
+require("hardhat-circom");
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.11",
+      },
+      {
+        version: "0.8.9",
+      },
+    ],
+  },
+  circom: {
+    inputBasePath: "./circuits",
+    ptau: "https://hermezptau.blob.core.windows.net/ptau/powersOfTau28_hez_final_15.ptau",
+    circuits: [
+      {
+        name: "init"
+      },
+    ],
+  },
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: [`0x` + process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_KEY}`
+  }
+};
